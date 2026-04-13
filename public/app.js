dfae4e11c5b3faa2c449e98759e7de01e4aa64bf
jeffersonpeixoto <jeffersonpeixoto23@gmail.com>
2026-04-12 21:15:07 -0300

    Add files via upload

--- public/app.js
+++ public/app.js
@@ -589,7 +589,7 @@ async function enviarWhatsApp(mensagem) {
   const instanceId = "3F18330F3791724F480CBE4FDF68D33E";
   const token = "589BC2DFE42C57968A672E6D";
 
-  const telefone = "5586998525536,5586994802019";
+  const telefone = "5586998110012";
 
   try {
     const response = await fetch(`https://api.z-api.io/instances/${instanceId}/token/${token}/send-text`, {
@@ -599,14 +599,14 @@ async function enviarWhatsApp(mensagem) {
       },
       body: JSON.stringify({
         phone: telefone,
-        message: mensagem
+        message: "teste"
       })
     });
 
-    console.log("STATUS HTTP:", response.status);
-
     const result = await response.json();
-    console.log("Z-API retorno:", result);
+
+    console.log("STATUS:", response.status);
+    console.log("RESPOSTA COMPLETA:", result);
 
   } catch (err) {
     console.error("Erro WhatsApp:", err);
